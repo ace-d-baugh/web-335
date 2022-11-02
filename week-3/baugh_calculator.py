5a733f3ac5b02010d00b0f744f280889c371916f
Ace Baugh <ace.d.baugh@gmail.com>
2022-11-02 17:33:50 -0400

    Finished python calculator

---
+++ week-3/baugh_calculator.py
@@ -0,0 +1,41 @@
+"""
+Title: myworld.py
+Author: Ace Baugh
+Date: 11/2/22
+Description: Exercise 3.3 - Python Variables and Functions
+"""
+
+# function to add two numbers
+def add(add1, add2):
+   return add1 + add2
+
+# function to subtract two numbers
+def subtract(subtract1, subtract2):
+   return subtract1 - subtract2
+
+# function to divide two numbers
+def divide(divide1, divide2):
+   return divide1 / divide2
+   
+# function to multiply two numbers
+def multiply(multiply1, multiply2):
+   return multiply1 * multiply2
+
+#creating test variables
+add1 = 4
+add2 = 4
+subtract1 = 10
+subtract2 = 6
+divide1 = 8
+divide2 = 2
+multiply1 = 10
+multiply2 = 2
+
+# printing test variables to the screen in easy to read format
+#concatenating strings and variables
+print(str(add1) + " + " + str(add2) + " is " + str(add(add1, add2)) + ".")
+print(str(subtract1) + " - " + str(subtract2) + " is " + str(subtract(subtract1, subtract2)) + ".")
+print(str(divide1) + " / " + str(divide2) + " is " + str(divide(divide1, divide2)) + ".")
+print(str(multiply1) + " * " + str(multiply2) + " is " + str(multiply(multiply1, multiply2)) + ".")
+
+
