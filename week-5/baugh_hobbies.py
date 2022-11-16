99f3e9a1c0c7cf4ac384a581335b0474b4ae9c34
Ace Baugh <ace.d.baugh@gmail.com>
2022-11-16 16:13:47 -0500

    5.3 done!

---
+++ week-5/baugh_hobbies.py
@@ -0,0 +1,30 @@
+"""
+Title: baugh_hobbies.py
+Author: Ace Baugh
+Date: 11/16/22
+Description: Exercise 5.3 - Python Conditionals, Lists, and Loops
+"""
+
+# List of hobbies
+hobbies = ["photography", "origami", "cooking", "camping", "park hopping", "crocheting"]
+
+# function to choose a random hobby
+def choose_hobby():
+   import random
+   return random.choice(hobbies)
+
+# for loop to print each hobby
+for hobby in hobbies:
+   print(hobby)
+
+# days of the week
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# for loop to print each day
+for day in days:
+   # if statement to print if day is a weekend: Saturday or Sunday is a time for hobby else print it is a work day
+   if day == "Saturday" or day == "Sunday":
+      # concatenate string with hobby
+      print(day + " is a day off: I am free to enjoy " + choose_hobby() + ".")
+   else:
+      print(day + " is a work day: I am busy with work.")
